a393dba4cf6fc2718b204f841d113bbdb48096f7
zivd <ziad99kr@gmail.com>
2026-03-01 01:31:23 +0100

    add tailwind config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div >
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
