df63611523740da3abbdfb3c707339e6cc5f19bf
zivd <ziad99kr@gmail.com>
2026-03-02 01:21:17 +0100

    add function hook to get llm response

---
+++ backend/src/logic.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import 'dotenv/config'
+ 
+
+
+const API_URL = process.env.API_URL;
+const API_KEY = process.env.API_KEY;
+
+
+async function askLLM(prompt) {
+  const response = await axios.post(API_URL, {
+    model: "llama-3.3-70b-versatile",
+    messages: [{ role: "user", content: prompt }],
+    temperature: 0.3  
+  }, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${API_KEY}`
+    }
+  });
+
+  const rawdata = response.data.choices[0].message.content;
+
+  try {
+  const cleanresponse = rawdata
+    .replace(/```json/g, '')
+    .replace(/```/g, '')
+    .trim();
+  return JSON.parse(cleanresponse);
+  } catch {
+    throw new Error(`llama returned invalid JSON:\n${raw}`);
+  }
+}
+ 
