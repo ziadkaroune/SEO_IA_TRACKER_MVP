989502e9cd63fa5067bb5d7c09d1fb10c12a1f57
zivd <ziad99kr@gmail.com>
2026-03-02 01:32:38 +0100

    main function pipline - analyzeVisibility()

--- backend/src/logic.js
+++ backend/src/logic.js
@@ -6,7 +6,7 @@ import 'dotenv/config'
 const API_URL = process.env.API_URL;
 const API_KEY = process.env.API_KEY;
 
-
+// FUNCTION TO CALL LLM ENDPOINT
 async function askLLM(prompt) {
   const response = await axios.post(API_URL, {
     model: "llama-3.3-70b-versatile",
@@ -20,7 +20,6 @@ async function askLLM(prompt) {
   });
 
   const rawdata = response.data.choices[0].message.content;
-
   try {
   const cleanresponse = rawdata
     .replace(/```json/g, '')
@@ -32,26 +31,34 @@ async function askLLM(prompt) {
   }
 }
 
-// pipline
 
-// function to get domaine name infos ( category , sector , competitors  ...)
+// le pipeline
+
+// 1 - Function to get domaine name infos ( category , sector , competitors  ...)
  async function getDomaineInfo(domain){
  const reponse  = await askLLM(prompt_domaine_info(domain));
  return reponse;
 }
 
-//function to simulate search engines quueries ( chatgpt , gemini , perplixity )
+// 2 - Function to simulate search engines quueries ( chatgpt , gemini , perplixity )
 async function simulationMoteurRecherche(domaine_info){
 const reponse = await askLLM(buildSimulationPrompt(domaine_info));
 return reponse
 
 }
-// analyse queries and add a system to calculate score and categorise the important ones
+//  3 - Analyse queries and add a system to calculate score and categorise the important ones
 async function score_system(domaine_info , sm_moteur_recherche){
 const response = await askLLM(buildScoringPrompt(domaine_info , sm_moteur_recherche));
 return response;
 }
 
+/// main function of the pipline
+export async function analyzeVisibility(domain){
 
+    const domaine_info = await getDomaineInfo(domain);
+    const sm_moteur_recherche = await simulationMoteurRecherche(domaine_info);
+    const scoresysytem = await score_system(domaine_info , sm_moteur_recherche);
+    return scoresysytem;
+}
 
 
